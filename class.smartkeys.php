eaca941f75babc977fd702d6b31af406860b26e3
dereksmart <smart@automattic.com>
2015-06-29 00:14:13 -0400

    passing over the organized data

--- class.smartkeys.php
+++ class.smartkeys.php
@@ -32,146 +32,54 @@ class Smartkeys {
 	}
 
 	/*
-	 * Get the top level admin pages
+	 * Convert menu_slug to menu title
 	 *
-	 * @return array
+	 * @param string $path
+	 *
+	 * @return string - the Title of the page.  Or WP_Error if something went wrong.
 	 */
-	function smartkeys_get_top_level_menu() {
+	function smartkeys_convert_to_title( $path = '' ) {
 		global $menu;
 
-		$menu_slugs = array();
-		// Get the top level menu as an array
-		foreach ( $menu as $k => $v ) {
-			// If a plugin adds a top-level menu, make sure we can get to it.
-			if ( ! strstr( $v[2], '.php' ) ) {
-				$v[2] = 'admin.php?page=' . $v[2];
-			}
+		if ( ! is_string( $path ) ) {
+			return new WP_Error( 'not-a-string', 'gotta use a string' );
+		}
 
-			// Some menu items have span tags in them, let's remove the number
-			if ( preg_match( '/(>\d{1,3}<)/', $v[0], $match ) ) {
-				$v[0] = wp_strip_all_tags( str_replace( $match, '', $v[0] ) );
-			}
-
-			// We don't want to add separators here.
-			if ( $v[4] !== 'wp-menu-separator' ) {
-				$menu_slugs[$v[2] . ' - ' . $v[0]]  = array(
-					'slug'    => $v[1],
-					'parent'  => $v[2],
-					'name'    => $v[0],
-					'command' => strtolower( $v[0] ),
-					'action'  => $v[2],
-				);
+		foreach ( $menu as $page => $values ) {
+			if ( $path == $values[2] ) {
+				return $values[0];
 			}
 		}
 
-		return $menu_slugs;
+		return 'Other';
 	}
 
-
 	/*
-	 * Get the sub pages in the admin
+	 * All of the admin pages in organized arrays
 	 *
 	 * @return array
 	 */
-	function smartkeys_get_sub_menu_pages() {
+	function smartkeys_organized_pages() {
 		global $submenu;
 
-		// Submenu slugs
-		$submenu_slugs = array();
-		if ( is_array( $submenu ) ) {
-			foreach ( $submenu as $k => $group ) {
-				foreach( $group as $g ) {
-					$submenu_slugs[$k. ' - ' . $g[0]] = array(
-						'slug'    => $g[1],
-						'parent'  => $k,
-						'name'    => $g[0],
-						'command' => strtolower( $g[0] ),
-						'action'  => $g[2],
-					);
-				}
-			}
-		}
-
-		return $submenu_slugs;
-	}
-
-	/*
-	 * Handle WordPress commands
-	 * @return array
-	 */
-	function smartkeys_prompt_commands() {
-		global $submenu;
+		$total = array();
 
 		foreach ( $submenu as $index => $pages ) {
 			$first = true;
+			$title = $this->smartkeys_convert_to_title( $index );
 
+			$group = array();
 			foreach ( $pages as $page ) {
 				if ( $first ) {
-					echo $index . ' - ';
+					echo $title . ' - ';
 					$first = false;
 				}
-
-				echo '<pre>';
-				print_r( $page );
-				echo '</pre>';
+				$group[ $page[0] ] = $page;
+				$total[ $title ] = $group;
 			}
 		}
 
-		exit;
-		/*
-		// Merge them all and sort
-		$all = array_merge_recursive( $menu_slugs, $submenu_slugs );
-		foreach ( $all as $row ) {
-			foreach ( $row as $key => $value ) {
-				${$key}[] = $value;
-			}
-		}
-
-//		array_multisort( $parent, SORT_ASC, $parenty, SORT_DESC, $all );
-		
-		// Only update in admin because global $menu is not available outside of admin
-		if ( is_admin() ) {
-			update_option( 'smartkey_commands', $all );
-		}
-
-		$default_commands = get_option( 'smartkey_commands' );
-
-		return $default_commands;
-		*/
-	}
-
-	/*
-	 * Jetpack specific pages
-	 *
-	 * @return array
-	 */
-	function smartkeys_jetpack_prompt_commands() {
-		if ( ! class_exists( 'Jetpack' ) ) {
-			return false;
-		}
-
-		if ( Jetpack::is_active() ) {
-			$modules = Jetpack::get_available_modules();
-
-			$module_name = array();
-			$settings_url = array();
-			foreach ( $modules as $module ) {
-				if ( Jetpack::module_configuration_url( $module ) ) {
-					$settings_url[] = Jetpack::module_configuration_url( $module );
-				}
-
-				$module_name[] = $module;
-			}
-		} else {
-			return false;
-		}
-
-		$jp_defaults = array(
-			'command' => $module_name,
-			'action'  => $settings_url
-		);
-
-		return $jp_defaults;
+		return $total;
 	}
 
 	function smartkeys_enqueue_admin_keys() {
@@ -185,8 +93,7 @@ class Smartkeys {
 			array(
 				'home_url'         => home_url(),
 				'option_keycodes'  => get_option( 'keys_to_save' ),
-				'prompt_commands'  => $this->smartkeys_prompt_commands(),
-				'jetpack_commands' => $this->smartkeys_jetpack_prompt_commands(),
+				'prompt_commands'  => $this->smartkeys_organized_pages(),
 				'currentCombo'   => '',
 			)
 		);
