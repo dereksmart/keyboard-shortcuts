dc926185a5373d1237830e59bef0be050458366e
dereksmart <smart@automattic.com>
2015-06-28 20:26:31 -0400

    Organize some stuff

--- class.smartkeys.php
+++ class.smartkeys.php
@@ -31,40 +31,50 @@ class Smartkeys {
 		wp_register_style( 'smartkeys-css', plugins_url( 'css/style.css' , __FILE__ ) );
 	}
 
-
 	/*
-	 * Handle WordPress commands
+	 * Get the top level admin pages
+	 *
 	 * @return array
 	 */
-	function smartkeys_prompt_commands() {
-		global $menu, $submenu;
+	function smartkeys_get_top_level_menu() {
+		global $menu;
 
-		$menu_slugs  = array();
-		if ( is_array( $menu ) ) {
-			foreach ( $menu as $k => $v ) {
-				// If a plugin adds a top-level menu, make sure we can get to it.
-				if ( ! strstr( $v[2], '.php' ) ) {
-					$v[2] = 'admin.php?page=' . $v[2];
-				}
+		$menu_slugs = array();
+		// Get the top level menu as an array
+		foreach ( $menu as $k => $v ) {
+			// If a plugin adds a top-level menu, make sure we can get to it.
+			if ( ! strstr( $v[2], '.php' ) ) {
+				$v[2] = 'admin.php?page=' . $v[2];
+			}
 
-				// Some menu items have span tags in them, let's remove the number
-				if ( preg_match( '/(>\d{1,3}<)/', $v[0], $match ) ) {
-					$v[0] = wp_strip_all_tags( str_replace( $match, '', $v[0] ) );
-				}
+			// Some menu items have span tags in them, let's remove the number
+			if ( preg_match( '/(>\d{1,3}<)/', $v[0], $match ) ) {
+				$v[0] = wp_strip_all_tags( str_replace( $match, '', $v[0] ) );
+			}
 
-				// We don't want to add separators here.
-				if ( $v[4] !== 'wp-menu-separator' ) {
-					$menu_slugs[$v[2] . ' - ' . $v[0]]  = array(
-//						'slug'    => $v[1],
-						'parent'  => $v[2],
-						'parenty' => 1,
-						'name'    => $v[0],
-						'command' => strtolower( $v[0] ),
-						'action'  => $v[2],
-					);
-				}
+			// We don't want to add separators here.
+			if ( $v[4] !== 'wp-menu-separator' ) {
+				$menu_slugs[$v[2] . ' - ' . $v[0]]  = array(
+					'slug'    => $v[1],
+					'parent'  => $v[2],
+					'name'    => $v[0],
+					'command' => strtolower( $v[0] ),
+					'action'  => $v[2],
+				);
 			}
 		}
+
+		return $menu_slugs;
+	}
+
+
+	/*
+	 * Get the sub pages in the admin
+	 *
+	 * @return array
+	 */
+	function smartkeys_get_sub_menu_pages() {
+		global $submenu;
 
 		// Submenu slugs
 		$submenu_slugs = array();
@@ -72,9 +82,8 @@ class Smartkeys {
 			foreach ( $submenu as $k => $group ) {
 				foreach( $group as $g ) {
 					$submenu_slugs[$k. ' - ' . $g[0]] = array(
-//						'slug' => $g[1],
-						'parent' => $k,
-						'parenty' => 0,
+						'slug'    => $g[1],
+						'parent'  => $k,
 						'name'    => $g[0],
 						'command' => strtolower( $g[0] ),
 						'action'  => $g[2],
@@ -83,6 +92,21 @@ class Smartkeys {
 			}
 		}
 
+		return $submenu_slugs;
+	}
+
+	/*
+	 * Handle WordPress commands
+	 * @return array
+	 */
+	function smartkeys_prompt_commands() {
+		global $submenu;
+
+		echo '<pre>';
+		print_r( $this->smartkeys_get_sub_menu_pages() );
+		echo '</pre>';
+		exit;
+		/*
 		// Merge them all and sort
 		$all = array_merge_recursive( $menu_slugs, $submenu_slugs );
 		foreach ( $all as $row ) {
@@ -91,7 +115,7 @@ class Smartkeys {
 			}
 		}
 
-		array_multisort( $parent, SORT_ASC, $parenty, SORT_DESC, $all );
+//		array_multisort( $parent, SORT_ASC, $parenty, SORT_DESC, $all );
 		
 		// Only update in admin because global $menu is not available outside of admin
 		if ( is_admin() ) {
@@ -101,8 +125,14 @@ class Smartkeys {
 		$default_commands = get_option( 'smartkey_commands' );
 
 		return $default_commands;
+		*/
 	}
 
+	/*
+	 * Jetpack specific pages
+	 *
+	 * @return array
+	 */
 	function smartkeys_jetpack_prompt_commands() {
 		if ( Jetpack::is_active() ) {
 			$modules = Jetpack::get_available_modules();
@@ -135,7 +165,7 @@ class Smartkeys {
 			array(
 				'home_url'         => home_url(),
 				'option_keycodes'  => get_option( 'keys_to_save' ),
-				'prompt_commands'  => $this->smartkeys_prompt_commands(),
+//				'prompt_commands'  => $this->smartkeys_prompt_commands(),
 				'jetpack_commands' => $this->smartkeys_jetpack_prompt_commands(),
 				'currentCombo'   => '',
 			)
